878d146d7ea27bf69d61ea0e0823449a57e0a1cb
Vika Kuznetsova <vika@kiit.net>
2016-07-31 20:11:19 +0300

    Changes

--- js/HW1-2/1/script.js
+++ js/HW1-2/1/script.js
@@ -1,1 +1,21 @@
-alert("Hello, world!");
+
+
+function pow(x,n) {
+	var answer=1;
+
+	for (var i = 1; i <= n; i++) {
+		answer = answer * x;
+	}
+
+	return answer;
+}
+	
+	alert('Вычисляем x^n');
+	var x=prompt('Введите x', '');
+	var n=prompt('Введите n', '');
+
+	var calcPow;
+
+	calcPow = pow(x, n);
+
+	console.log('calcPow', calcPow);
