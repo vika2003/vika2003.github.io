d04c236323a12289a2bf69ce8a7e2d62f3aaa5de
Vika Kuznetsova <vika@kiit.net>
2016-08-06 11:04:02 +0300

    Added JS homeworks

--- js/HW1-2/1/script.js
+++ js/HW1-2/1/script.js
@@ -1,21 +1,31 @@
+alert('Вычисляем x^n');
+	var x = prompt('Введите x', '');
+	var n = prompt('Введите n', '');
 
-
-function pow(x,n) {
+	
+function pow(x, n) {
 	var answer=1;
 
 	for (var i = 1; i <= n; i++) {
 		answer = answer * x;
+
+	}
+
+	if (n < 0) {
+		var n = -n;
+	for (var i = 1; i <= n; i++) {
+		answer = answer * (1/x);
+	}
 	}
 
 	return answer;
 }
 	
-	alert('Вычисляем x^n');
-	var x=prompt('Введите x', '');
-	var n=prompt('Введите n', '');
-
 	var calcPow;
 
 	calcPow = pow(x, n);
 
 	console.log('calcPow', calcPow);
+	alert(calcPow);
+
+
---
+++ js/HW1-2/2/script.js
@@ -0,0 +1,28 @@
+var names = [];
+
+for (var i = 0; i < 5; i++) {
+	names[i] = prompt('Введите имя', '');
+
+}
+
+var user = prompt('Введите имя пользователя', '');
+	
+	if (user.length === 0) {
+		alert('Вы не ввели имя пользователя');
+		user = prompt('Введите имя пользователя', '');
+	} 
+
+var flag = 0;
+
+for (var i = 0; i < 5; i++) {
+	if (user == names[i]) {
+		flag = 1;
+		break;
+	}  
+}
+
+	if (flag == 1) {
+		alert(user + ', вы успешно вошли!');
+	}  else {
+		alert('В списке нет пользователя с таким именем');
+	}
